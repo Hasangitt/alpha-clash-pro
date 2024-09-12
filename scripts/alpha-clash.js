d91ebb83f06f831ff279795686d2cc49f7c9ffa2
Hasangitt <pingpong038759@gmail.com>
2024-09-13 00:16:12 +0600

    adding javascript

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,29 @@
 function play(){
-    console.log('connected')
+    //******* */ home sreen remove********
+    const homeScreen = document.getElementById('home-screen');
+    homeScreen.classList.add('hidden');
+    //******* */ playground sreen add********
+    const playgroundScreen = document.getElementById('playground-screen');
+    playgroundScreen.classList.remove('hidden');
+
+    //********/ alphabets*************
+    const alphabetStrings = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetStrings.split('');
+    // ***** */ get alphabet***********
+    const randomAlphabets = Math.random()*25;
+    const index = Math.round(randomAlphabets);
+    const alphabet = alphabets[index];
+    
+    // ********* set alphabet***********
+    const currentAlphabet = document.getElementById('current-alphabet');
+    currentAlphabet.innerText = alphabet;
+
+    // **********set keyboard bg color *************
+    setBackgroundColor(alphabet);
+
+}
+
+function setBackgroundColor(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-600');
 }
