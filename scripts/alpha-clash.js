98b61a1530c93d1d1f15987d86eb3a2a020d96ef
Hasangitt <pingpong038759@gmail.com>
2024-09-14 11:13:17 +0600

    continue game, clear alphabet bg

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,25 +1,7 @@
+
+
 function play(){
-    //******* */ home sreen remove********
-    const homeScreen = document.getElementById('home-screen');
-    homeScreen.classList.add('hidden');
-    //******* */ playground sreen add********
-    const playgroundScreen = document.getElementById('playground-screen');
-    playgroundScreen.classList.remove('hidden');
-
-    //********/ alphabets*************
-    const alphabetStrings = 'abcdefghijklmnopqrstuvwxyz';
-    const alphabets = alphabetStrings.split('');
-    // ***** */ get alphabet***********
-    const randomAlphabets = Math.random()*25;
-    const index = Math.round(randomAlphabets);
-    const alphabet = alphabets[index];
-    
-    // ********* set alphabet***********
-    const currentAlphabet = document.getElementById('current-alphabet');
-    currentAlphabet.innerText = alphabet;
-
-    // **********set keyboard bg color *************
-    setBackgroundColor(alphabet);
-
+    screenRemove();
+    screenAdd();
+    continueGame();
 }
-
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,7 +1,44 @@
+function screenRemove(){
+    //******* */ home sreen remove********
+    const homeScreen = document.getElementById('home-screen');
+    homeScreen.classList.add('hidden');
+}
+
+function screenAdd(){
+     //******* */ playground screen add********
+     const playgroundScreen = document.getElementById('playground-screen');
+     playgroundScreen.classList.remove('hidden');
+}
+
+
+// alphabet container
+function continueGame(){
+     //********/set random alphabets element*************
+     const alphabetStrings = 'abcdefghijklmnopqrstuvwxyz';
+     const alphabets = alphabetStrings.split('');
+ 
+     // ***** */ get random alphabet***********
+     const randomAlphabets = Math.random()*25;
+     const index = Math.round(randomAlphabets);
+     const alphabet = alphabets[index];
+     
+     // ********* get screen alphabet***********
+     const currentAlphabet = document.getElementById('current-alphabet');
+     currentAlphabet.innerText = alphabet;
+    //  ********** alphabet bg color***********
+     setBackgroundColor(alphabet);
+}
+
+
 // set background color in alphabet
 function setBackgroundColor(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-600');
+}
+// remove background color in alphabet
+function removeBackgroundColor(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-600');
 }
 
 // keyboard button press
@@ -9,7 +46,7 @@ function handleKeyboradButtonPress(event){
     const playerPress = event.key;
     console.log('player press', playerPress);
 
-    // get expected alphabet
+    // get or set expected alphabet
     const getExpectedAlphabetElement = document.getElementById('current-alphabet');
     const expectedCureentAlphabet  = getExpectedAlphabetElement.innerText;
     const expectedAlphabet = expectedCureentAlphabet.toLowerCase();
@@ -18,6 +55,8 @@ function handleKeyboradButtonPress(event){
     // check matched or not
     if(playerPress === expectedAlphabet){
         console.log('you got a point');
+        removeBackgroundColor(expectedAlphabet);
+        continueGame();
     }
    
     else{
@@ -25,5 +64,5 @@ function handleKeyboradButtonPress(event){
     }
 
 }
-
+// key press listener
 document.addEventListener('keyup', handleKeyboradButtonPress);
